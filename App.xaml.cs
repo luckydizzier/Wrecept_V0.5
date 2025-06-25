3ee13aac4d406758f93ca0721235fd4b8ca0f1f9
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-06-25 03:43:26 +0200

    CodeGen-CSharp build: fix compile errors

--- App.xaml.cs
+++ App.xaml.cs
@@ -19,8 +19,8 @@ namespace Wrecept
         public static void ApplyTheme(string theme)
         {
             var dict = new ResourceDictionary { Source = new Uri($"Themes/{theme}.xaml", UriKind.Relative) };
-            Resources.MergedDictionaries.Clear();
-            Resources.MergedDictionaries.Add(dict);
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(dict);
         }
     }
 
