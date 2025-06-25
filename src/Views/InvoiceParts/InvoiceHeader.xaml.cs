be1c9bb626f714cef03cb004da426328d7d70f80
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-06-26 00:52:19 +0200

    [CodeGen-CSharp] InvoiceParts: add code-behind stubs

---
+++ src/Views/InvoiceParts/InvoiceHeader.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Wrecept.Views.InvoiceParts;
+
+public partial class InvoiceHeader : UserControl
+{
+    public InvoiceHeader()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Views/InvoiceParts/InvoiceSidebar.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Wrecept.Views.InvoiceParts;
+
+public partial class InvoiceSidebar : UserControl
+{
+    public InvoiceSidebar()
+    {
+        InitializeComponent();
+    }
+}
