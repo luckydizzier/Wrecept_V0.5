2ddd5dfa827ff93837aa5390a16e40663bae5976
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-06-26 22:00:33 +0200

    CodeGen-CSharp inline: handle enter and escape

--- src/Views/Inline/InlineProductCreator.xaml.cs
+++ src/Views/Inline/InlineProductCreator.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows.Controls;
+using System.Windows.Input;
+using Wrecept.ViewModels;
 
 namespace Wrecept.Views.Inline;
 
@@ -7,5 +9,24 @@ public partial class InlineProductCreator : UserControl
     public InlineProductCreator()
     {
         InitializeComponent();
+    }
+
+    private void UserControl_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (DataContext is InlineProductCreatorViewModel vm)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (vm.SaveCommand.CanExecute(null))
+                    vm.SaveCommand.Execute(null);
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Escape)
+            {
+                if (vm.CancelCommand.CanExecute(null))
+                    vm.CancelCommand.Execute(null);
+                e.Handled = true;
+            }
+        }
     }
 }
