27a3beb9914040fea4cec71a6c403eeb2f96b7c8
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-06-27 21:17:09 +0200

    [CodeGen-XAML] fix: remove invalid AccessKey property

--- src/Views/InvoiceParts/InvoiceHeader.xaml.cs
+++ src/Views/InvoiceParts/InvoiceHeader.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using System.Windows.Input;
 using Wrecept.ViewModels;
 
 namespace Wrecept.Views.InvoiceParts;
@@ -8,6 +9,16 @@ public partial class InvoiceHeader : UserControl
     public InvoiceHeader()
     {
         InitializeComponent();
-        Loaded += (_, _) => SupplierNameBox.SearchBox.Focus();
+        Loaded += (_, _) =>
+        {
+            SupplierNameBox.SearchBox.Focus();
+            AccessKeyManager.Register("N", SupplierNameBox);
+            AccessKeyManager.Register("C", AddressBox);
+            AccessKeyManager.Register("A", TaxNumberBox);
+            AccessKeyManager.Register("P", SerialBox);
+            AccessKeyManager.Register("D", IssueDateBox);
+            AccessKeyManager.Register("M", PaymentMethodBox);
+            AccessKeyManager.Register("T", TransactionBox);
+        };
     }
 }
